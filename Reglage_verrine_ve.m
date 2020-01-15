1115fd270a7cd797cbab775224c20863db4fb913
ccatalano <camille.catalano@obs-vlfr.fr>
2020-01-15 15:57:34 +0100

    comments and outputs modif of reglage_verrine_ve, new names reglagexxxxxx.m

--- Reglage_verrine_ve.m
+++ Reglage_verrine_ve.m
@@ -1,7 +1,8 @@
 % ----------------------------------------------------------------------- %
-% --------------------- REGLAGE VERRINE HORIZONTAL ---------------------- %
+% --------------------- REGLAGE VERRINE VERTICAL ------------------------ %
 % ----------------------------------------------------------------------- %
 %
+% modified by Camille Catalano
 % Editor : Louis Petiteau - petiteau@ensta.fr
 % Date : 28/11/2018
 
@@ -30,7 +31,7 @@ function Reglage_verrine_ve
     end
 
 
-%% Octopus Characterisation of sensibility
+%% Initialisation
 
 clear all
 close all
@@ -68,21 +69,21 @@ min_poly_slope = 1000;
 N = 20;
 
 %Calculate the space discretisation
-pas = floor((2464-350-350)/(2*N));
+step = floor((2464-350-350)/(2*N));
 
-%% ACQUISITION PARAMETERS
+% acquisition parameters
 zonal_cor = 'off';
 
 
-%% Select orginal image
 %Get the directory where the images will be stored
-img_folder = uigetdir('Select image folder');
+img_folder = uigetdir([],'Select images folder');
 cd(img_folder);
 
-octopus_type = 'Vertical';
+UVP_light_type = 'Vertical';
 % ENTER NEW PIXEL SIZE
 pixel = input('Input the pixel size in mm calculated from the reference image (0.0933) :');
 
+%% build the GUI
 %Open figure full size
 f=figure;
 set(gcf, 'Position', get(0, 'Screensize'));
@@ -115,7 +116,7 @@ B.Units ='normalized';
 B.Position = [pos(1)-0.1 pos(2)-0.02 0.045 0.045];
 
 
-
+%Set the best angle and best ET boxes
 Button1 = uicontrol('Style', 'PushButton','String', 'Best angle');
 Button1.Visible = 'off';
 pos = get(subplot(2,3,1),'position');
@@ -132,8 +133,6 @@ Button2.Position = [pos(1)-0.1 pos(2)-0.05 0.05 0.05];
 Button2.BackgroundColor = 'r';
 
 %% INFINITE LOOP
-
-
 % While loop which stops when clicking on "settings ok"
 while fin == hh | last_image <= 10
     base_grey = dir(img_folder);
@@ -190,7 +189,7 @@ while fin == hh | last_image <= 10
             dist = (size(img,2)-650);
             angle_deg = atand(abs(shift)/dist);
             
-            % Angle warning
+            % Angle warning if the beam is not close to the horizontal
             if abs(angle_deg) > angle_limit
                 disp('ANGLE of light beam > 2°, results may be biased')
             end
@@ -219,7 +218,7 @@ while fin == hh | last_image <= 10
             if last_image == 9
             disp(mid)
             end
-            if mid+10>mid+pas-10
+            if mid+10>mid+step-10
                 disp('N trop grand, diminuer N ou diminuer la taille de la tranche d''intégration (actuellement 20)')
             end
             
@@ -288,17 +287,15 @@ while fin == hh | last_image <= 10
             indice(N+1,3) = (intensity_l-intensity_r)/total_intensity;
             thickness(N+1) = size(inter,2);
             
-            
+            %% loop on the vertical profiles
             %if loop that goes through the beam's lenghts and each step
-            %integrates a 21 pixels wide section spaced by pas pixels which
-            %a step calculated a the beginnng of the code
-            
+            %integrates a 21 pixels wide section spaced by step pixels which
+            %step is calculated at the beginnng of the code
             for i = 1 : N
-                
                 %% Left part of the beam
                 %each section is the mean of a 21 pixels wide section
-                img_crop_mean_orig(N+1-i,:) = mean(img(mid-pas*i-10:mid-pas*i+10,700:end-700));
-                intervalles(N+1-i,:) = [mid-pas*i-10:mid-pas*i+10];
+                img_crop_mean_orig(N+1-i,:) = mean(img(mid-step*i-10:mid-step*i+10,700:end-700));
+                intervalles(N+1-i,:) = [mid-step*i-10:mid-step*i+10];
                 
                 
                 aa = find(img_crop_mean_orig(N+1-i,:)>5);  %0.5*max(img_crop_mean_orig(N+1-i,:)));
@@ -339,8 +336,8 @@ while fin == hh | last_image <= 10
                 
                 %% Right part of the beam
                 %each section is the mean of a 21 pixels wide section
-                img_crop_mean_orig(N+1+i,:) = mean(img(mid+pas*i-10:mid+pas*i+10,700:end-700));
-                intervalles(N+1+i,:) = [mid+pas*i-10:mid+pas*i+10];
+                img_crop_mean_orig(N+1+i,:) = mean(img(mid+step*i-10:mid+step*i+10,700:end-700));
+                intervalles(N+1+i,:) = [mid+step*i-10:mid+step*i+10];
                 
                 aa = find(img_crop_mean_orig(N+1+i,:)> 5);%0.5*max(img_crop_mean_orig(N+1+i,:)));
                 limit_l = aa(1);
@@ -381,7 +378,8 @@ while fin == hh | last_image <= 10
             
             diff_max = 2*(max_l(:)-max_r(:))./(max_l(:)+max_r(:));
             
-            %Update of the matrixs in which the intressting parameters are
+            %% update of the table
+            %Update of the matrixs in which the interesting parameters are
             %stored
             ecart_type1 = sqrt(10000*indice(:,3)'*indice(:,3))/(2*N+1);
             ecart1 = [ecart1 ecart_type1];
@@ -402,8 +400,8 @@ while fin == hh | last_image <= 10
             poly2 = polyfit(intervalles([1:2*N+1],11)*pixel,diff_max,1);
             diff_max_poly = poly2(1)*intervalles([1:2*N+1],11)*pixel+poly2(2);
             %Angle related to this rotation
-            diff_peak_angle = atand((diff_peak_poly(N+1+floor(N/2))-diff_peak_poly(N+1))/(floor(N/2)*pas));
-            diff_max_angle = atand((diff_max_poly(N+1+floor(N/2))-diff_max_poly(N+1))/(floor(N/2)*pas));
+            diff_peak_angle = atand((diff_peak_poly(N+1+floor(N/2))-diff_peak_poly(N+1))/(floor(N/2)*step));
+            diff_max_angle = atand((diff_max_poly(N+1+floor(N/2))-diff_max_poly(N+1))/(floor(N/2)*step));
             
             poly_slope = [poly_slope poly1(1)];
             
@@ -434,7 +432,7 @@ while fin == hh | last_image <= 10
                 Button1.Visible = 'off';
             end
             
-            
+            %% Plot section
             %Set the previous plots'colors to green as the 'background'
             if hh == 0
                 plot1.Color = 'g';
@@ -451,13 +449,13 @@ while fin == hh | last_image <= 10
                 delete(plot8)
             end
             
-            %% Plot section
+            %new plots
             subplot(2,3,1)
             plot3 = plot(intervalles([1:2*N+1],11)*pixel,indice(:,3),'b');
             hold on
-            plot([mid-pas*N-100 mid+pas*N+100]*pixel,[0,0],'r')
+            plot([mid-step*N-100 mid+step*N+100]*pixel,[0,0],'r')
             plot6 = plot(intervalles([1:2*N+1],11)*pixel,diff_peak_poly,'k');
-            title({'fig 1 - Intensity diffrence between upper and lower part of ','the beam divided by the total intensity ZOOM'})
+            title({'fig 1 - Intensity difference between upper and lower part of ','the beam divided by the total intensity ZOOM'})
             xlabel('Lenght of the beam (mm)')
             ylabel('Normalized intensity')
             axis([0 2464*pixel -0.08 0.08])
@@ -470,13 +468,13 @@ while fin == hh | last_image <= 10
             subplot(2,3,2)
             plot2 = plot(intervalles([1:2*N+1],11)*pixel,diff_max,'b');
             hold on
-            plot([mid-pas*N-100 mid+pas*N+100]*pixel,[0 0],'r')
+            plot([mid-step*N-100 mid+step*N+100]*pixel,[0 0],'r')
             plot5 = plot(intervalles([1:2*N+1],11)*pixel,diff_max_poly,'k');
-            title({'fig2 - Diffrence between max of the bottom','and top peaks divided by the mean'})
+            title({'fig3 - Difference between max of the bottom','and top peaks divided by the mean'})
             xlabel('Lenght of the beam (mm)')
             ylabel('Normalized intensity')
             axis([0 2464*pixel -0.8 0.8])
-            text2 = text(20,-0.7,['pente :' num2str(1000*poly2(1),'%.3g')]);
+            text2 = text(20,-0.7,['slope :' num2str(1000*poly2(1),'%.3g')]);
             
             subplot(2,3,5)
             plot1 = plot(intervalles([1:2*N+1],11)*pixel,thickness*pixel,'b');
@@ -493,12 +491,12 @@ while fin == hh | last_image <= 10
             xlabel('Lenght of the beam (mm)')
             ylabel('Intensity')
             
-             subplot(2,3,4)
+            subplot(2,3,4)
             plot7 = plot(intervalles([1:2*N+1],11)*pixel,indice(:,3),'b');
             hold on
-            plot([mid-pas*N-100 mid+pas*N+100]*pixel,[0,0],'r')
+            plot([mid-step*N-100 mid+step*N+100]*pixel,[0,0],'r')
             plot8 = plot(intervalles([1:2*N+1],11)*pixel,diff_peak_poly,'k');
-            title({'fig 2 - Intensity diffrence between upper and lower part of ','the beam divided by the total intensity'})
+            title({'fig 2 - Intensity difference between upper and lower part of ','the beam divided by the total intensity'})
             xlabel('Lenght of the beam (mm)')
             ylabel('Normalized intensity')
             
@@ -529,25 +527,23 @@ while cc == 0
     pause(0.3)
 end
 
+%% save results
 %If we've decided to save figures by clicking on the Save Figure button
 if cc == 2
-    
     %% Ask metadata
-    
-    octopus_cam = input('Input Octopus CAMERA serial number (xxx) ','s');
-    octopus_light = input('Input Octopus LIGHT serial number (xxx) ','s');
-    people = input('Enter people first and last name (default : Marc Picheral) ','s');
-    if isempty(people); people = 'Marc Picheral'; end
-    shutter = input('Input exposure setting of the livecamera acqusition ','s');
+    UVP_cam = input('Enter CAMERA serial number (xxx) ','s');
+    UVP_light = input('Enter LIGHT serial number (xxx) ','s');
+    operator = input('Enter operator first and last name (default : Marc Picheral) ','s');
+    if isempty(operator); operator = 'Marc Picheral'; end
+    shutter = input('Enter exposure setting of the livecamera acqusition (default : 80µs)','s');
     if isempty(shutter); shutter = '80'; end
-    gain = input('Input gain setting of the livecamera acquisition ','s');
+    gain = input('Enter gain setting of the livecamera acquisition (default : 0)','s');
     if isempty(gain); gain = '0'; end
 %     suppl = input('Input supplement for the name of the text and graph file ','s');
 %     if isempty(suppl); suppl = ''; end
     
     
     %% Modify figure
-    
     Button1.Visible = 'off';
     Button2.Visible = 'off';
     delete(t)
@@ -566,66 +562,68 @@ if cc == 2
     hold on
     
     for i = 1:N
-        plot([mid+pas*i mid+pas*i],[150 size(img_rot,1)-150],'g-')
-        plot([mid-pas*i mid-pas*i],[150 size(img_rot,1)-150],'g-')
+        plot([mid+step*i mid+step*i],[150 size(img_rot,1)-150],'g-')
+        plot([mid-step*i mid-step*i],[150 size(img_rot,1)-150],'g-')
     end
     
 
-    %% Save Results
-    path = uigetdir('Select save folder');
+    %% Save results in txt file
+    path = uigetdir([], 'Select save folder');
     cd(path);
-    mkdir([datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' octopus_light]);
-    cd([datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' octopus_light]);
+    mkdir([datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' UVP_light]);
+    cd([datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' UVP_light]);
     
-    fid_uvp = fopen(fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' octopus_light '.txt']),'w');
+    fid_uvp = fopen(fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrines_ve_' UVP_light '.txt']),'w');
     fprintf(fid_uvp,'%s\r','WARNING : AIR measurements and references !');
     fprintf(fid_uvp,'%s\r',['Processing date                : ',datestr(now,31)]);
     fprintf(fid_uvp,'%s\r',['Image acquisition date         : ',datestr(base_grey(end).datenum,31)]);
-    fprintf(fid_uvp,'%s\r',['Octopus CAMERA s/n             : ',num2str(octopus_cam)]);
-    fprintf(fid_uvp,'%s\r',['Octopus LIGHT s/n              : ',num2str(octopus_light)]);
-    fprintf(fid_uvp,'%s\r',['Octopus LIGHT type             : ',char(octopus_type)]);
+    fprintf(fid_uvp,'%s\r',['Operator                       : ',operator]);
+    fprintf(fid_uvp,'%s\r',['UVP CAMERA s/n             : ',num2str(UVP_cam)]);
+    fprintf(fid_uvp,'%s\r',['UVP LIGHT s/n              : ',num2str(UVP_light)]);
+    fprintf(fid_uvp,'%s\r',['UVP LIGHT type             : ',char(UVP_light_type)]);
     fprintf(fid_uvp,'%s\r',['Livecamera shutter             : ',shutter]);
     fprintf(fid_uvp,'%s\r',['Livecamera gain                : ',gain]);
     fprintf(fid_uvp,'%s\r',['BEAM ANGLE [degre]             : ',num2str(round(angle_deg,2))]);
-    fprintf(fid_uvp,'%s\r',['Octopus pixel in air [mm]      : ',num2str(pixel)]);
-    fprintf(fid_uvp,'%s\r',['Octopus mean thickness L [mm]  : ',num2str(pixel*mean(thickness(end-9:end)))]);
-    fprintf(fid_uvp,'%s\r',['Octopus max Intensity          : ',num2str(round(mean(max_int(end-9:end)),2))]);
-    fprintf(fid_uvp,'%s\r',['Octopus mean Intensity         : ',num2str(round(mean(mean_int(end-9:end)),2))]);
-    fprintf(fid_uvp,'%s\r',['Octopus total Intensity        : ',num2str(int_tot)]);
-    fprintf(fid_uvp,'%s\r',['Octopus angle of Intensity diffrence between upper and lower part of the beam       : ',num2str((mean(poly_slope(end-9:end))))]);
-    fprintf(fid_uvp,'%s\r',['Octopus standard deviation of Intensity difference between upper and lower part of the beam : ',num2str(round(mean(ecart1(end-9:end))*10,3))]);
+    fprintf(fid_uvp,'%s\r',['Pixel size in air [mm]      : ',num2str(pixel)]);
+    fprintf(fid_uvp,'%s\r',['Mean thickness L [mm]  : ',num2str(pixel*mean(thickness(end-9:end)))]);
+    fprintf(fid_uvp,'%s\r',['Max Intensity          : ',num2str(round(mean(max_int(end-9:end)),2))]);
+    fprintf(fid_uvp,'%s\r',['Mean Intensity         : ',num2str(round(mean(mean_int(end-9:end)),2))]);
+    fprintf(fid_uvp,'%s\r',['Total Intensity        : ',num2str(int_tot)]);
+    fprintf(fid_uvp,'%s\r',['Angle of Intensity diffrence between upper and lower part of the beam       : ',num2str((mean(poly_slope(end-9:end))))]);
+    fprintf(fid_uvp,'%s\r',['Standard deviation of Intensity difference between upper and lower part of the beam : ',num2str(round(mean(ecart1(end-9:end))*10,3))]);
     
     
     
 %     fprintf(fid_uvp,'%s\r',['REFERENCE THICNESS [mm]  : ',num2str(ref_thick_air)]);
 %     fprintf(fid_uvp,'%s\r',['REFERENCE MEAN INTENSITY : ',num2str(ref_int)]);
     
-    saveas(f,fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrives_' octopus_light '_graphs.png']))
+    %% save full figure
+    saveas(f,fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrives_' UVP_light '_graphs.png']))
     fclose(fid_uvp);
     close(f)
     
-    
+    %% save figure of max intensity vs time
     fig = figure;
 
-if max(max_int)-min(max_int)>1
-    N_min = size(max_int(find(max_int < min(max_int)+1)),2);
-    N_max = size(max_int(find(max_int > max(max_int)-1)),2);
-else
-    N_min = 0;
-    N_max = 0;
-end
+    if max(max_int)-min(max_int)>1
+        N_min = size(max_int(find(max_int < min(max_int)+1)),2);
+        N_max = size(max_int(find(max_int > max(max_int)-1)),2);
+    else
+        N_min = 0;
+        N_max = 0;
+    end
 
     
     plot(max_int,'x')
-    title(['Max intensity taken from the intensity profile with a suhtter = ' shutter])
-    xlabel('Number of image')
+    title('Max intensity taken from the intensity profile (shutter = ' + shutter + ')')
+    xlabel('Image number')
     ylabel('max intensity')
     fig.Units = 'normalized';
     
     annotation('textbox',[0.2 0.5 0.3 0.3],'String',['N' '_max = ' num2str(N_max)],'FitBoxToText','on')
     annotation('textbox',[0.2 0.2 0.3 0.3],'String',['N' '_min = ' num2str(N_min)],'FitBoxToText','on')   
     
-    saveas(fig,fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrives_' char(octopus_light) '_int_evolution.png']));
+    saveas(fig,fullfile(cd,[datestr(now,10) datestr(now,5) datestr(now,7) '_Reglage_verrives_' char(UVP_light) '_int_evolution.png']));
     copyfile([ img_folder '\' Image_name],cd,'f');
     
     
