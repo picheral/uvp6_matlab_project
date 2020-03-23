42a967a0a9a6e758400dc8388cf6cb866231474f
Marc Picheral <marc.picheral@obs-vlfr.fr>
2020-03-23 12:35:59 +0100

    Modification Datavalidation pour affichage similaire à Zooprocess des profils verticaux. Permet de comparer les nombres d'images après et avant FLAG de descente.

--- Traitement_donnees_projets/data_quality/DataValidation.m
+++ Traitement_donnees_projets/data_quality/DataValidation.m
@@ -1,4 +1,5 @@
-function validation = DataValidation(image_numbers, part, image_numbers_util, part_util, dat_pathname)
+function validation = DataValidation(listecor, dat_pathname)
+% function validation = DataValidation(image_numbers, part, image_numbers_util, part_util, dat_pathname)
     % DATA_VALIDATION user validation to know if the data are good
     % plots data and depth filtered data and ask if there are good
     %
@@ -12,18 +13,41 @@ function validation = DataValidation(image_numbers, part, image_numbers_util, pa
     %outputs:
     %   validation: y/n, good/bad data
     %
+    
     %% plot particules numbers along the sequence
-    subplot(2,1,1);
-    plot(image_numbers,part,'.');
-    xlabel('image number');
-    ylabel('number of particules');
+%     subplot(1,2,1);
+%     plot(image_numbers,part,'.');
+%     xlabel('image number');
+%     ylabel('number of particules');
+
     %% plot depth filtered particules numbers along the sequence
-    subplot(2,1,2)
-    plot(image_numbers_util,part_util,'.');
-    xlabel('depth filtered image number');
-    ylabel('number of particules');
+    subplot(1,2,1)
+    plot(listecor(:,4),-listecor(:,2),'b.');
+
+    %% Ajout moyenne mobile
+    hold on
+    plot(movavg(listecor(:,4),'linear',40),-listecor(:,2),'c-', 'LineWidth', 2);
+    
+    % Formattage du graph
+    xlabel(['TOTAL number of particles from ',num2str(numel(listecor(:,1))),' images']);
+    ylabel('Pressure');  
+ 
     sgtitle(regexprep(dat_pathname, {'\\', '\_'}, {'\\\\', '\\\_'}));
     set(gcf, 'Units', 'Normalized', 'OuterPosition', [0.1, 0.1, 0.9, 0.9]);
+    
+    % Filtrage par flag
+    subplot(1,2,2) 
+    aa= find(listecor(:,3)==1);
+    plot(listecor(aa,4),-listecor(aa,2),'b.');
+
+    %% Ajout moyenne mobile
+    hold on
+    plot(movavg(listecor(aa,4),'linear',40),-listecor(aa,2),'r-''LineWidth', 2);
+    
+    % Formattage du graph
+    xlabel(['TOTAL number of particles from ',num2str(numel(aa)),' images (Descent ONLY)']);
+    ylabel('Pressure');  
+    
     %% user validation
     validation = input('Is the data good ? ([y]/n) ', 's');
     if not(strcmp(validation, 'n'))
