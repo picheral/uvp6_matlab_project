b49d934f07e7a1572865dd74f2c8e7ee172fe71b
ccatalano <camille.catalano@obs-vlfr.fr>
2020-04-17 09:24:55 +0200

    mlx: print results for all references

--- intercalibration/calibration_uvp_functions/CalibrationUvpPrintResults.m
+++ intercalibration/calibration_uvp_functions/CalibrationUvpPrintResults.m
@@ -1,21 +1,22 @@
 %% UVP5 Results
 % Picheral Lombard 2017/11
 
-function CalibrationUvpPrintResults(process_params, ref_cast, adj_cast, aa_adj, expo_adj)
+function CalibrationUvpPrintResults(process_params, ref_cast_list, adj_cast, aa_adj, expo_adj)
 %CalibrationUvpPrintResults print ref and adj uvp parameters and results
 %
 %   inputs:
 %       process_params : process parameters
-%       ref_cast : struct storing computed variables from ref uvp
+%       ref_cast_list : list of struct storing computed variables from ref uvp
 %       adj_cast : struct storing computed variables from adj uvp
 %       aa_adj : aa parameters of size intercalibration of adj uvp
 %       expo_adj : expo parameters of size intercalibration of adj uvp
 %
 
-
+% process params
 disp('-------------------------------------------------------------------------');
 disp(['Processing date     : ',datestr(now,31)])
 disp('-------------------------------------------------------------------------');
+disp(['Nb of reference uvps: ',num2str(length(ref_cast_list))]);
 disp(['Min ESD       [mm]  : ',num2str(process_params.esd_min)]);
 disp(['Max ESD       [mm]  : ',num2str(process_params.esd_max)]);
 disp(['Mnimisation start   : [',num2str(process_params.X0(1)),' ',num2str(process_params.X0(2)),']']);
@@ -24,30 +25,36 @@ disp(['EC factor           : ',(num2str(process_params.EC_factor))]);
 disp(['EC                  : ',(num2str(process_params.Score))]);
 disp(['Ratio (after adjust): ',(num2str(process_params.ratio_mean))]);
 disp('-------------------------------------------------------------------------');
-aa_data_ref = ref_cast.aa_data;
-if contains(ref_cast.uvp,'uvp6'); aa_data_ref = round(aa_data_ref * 1000000);end  
-disp(['Reference UVP       : ',char(ref_cast.uvp)]);
-disp(['Light 1             : ',char(ref_cast.light1)]);
-disp(['Light 2             : ',char(ref_cast.light2)]);
-disp(['Reference folder    : ',char(ref_cast.project_folder(4:end))]);
-if (strcmp(ref_cast.project_folder(4:7),'uvp5'))
-    disp(['Reference profile   : ',char(ref_cast.histfile)]);
-else
-    disp(['Reference profile   : ',char(ref_cast.profilename)]);
+
+% ref uvp
+for i = 1:length(ref_cast_list)
+    aa_data_ref = ref_cast_list(i).aa_data;
+    if contains(ref_cast_list(i).uvp,'uvp6'); aa_data_ref = round(aa_data_ref * 1000000);end  
+    disp(['Reference UVP       : ',char(ref_cast_list(i).uvp)]);
+    disp(['Light 1             : ',char(ref_cast_list(i).light1)]);
+    disp(['Light 2             : ',char(ref_cast_list(i).light2)]);
+    disp(['Reference folder    : ',char(ref_cast_list(i).project_folder(4:end))]);
+    if (strcmp(ref_cast_list(i).project_folder(4:7),'uvp5'))
+        disp(['Reference profile   : ',char(ref_cast_list(i).histfile)]);
+    else
+        disp(['Reference profile   : ',char(ref_cast_list(i).profilename)]);
+    end
+    disp(['Reference profile # : ',num2str(ref_cast_list(i).record)]);
+    disp(['Observed volume [L] : ', num2str(sum(ref_cast_list(i).vol_ech(:,1), 'all'))]);
+    disp(['Shutter             : ',num2str(ref_cast_list(i).ShutterSpeed)]);
+    disp(['Gain                : ',num2str(ref_cast_list(i).gain)]);
+    disp(['Threshold           : ',num2str(ref_cast_list(i).Thres)]);
+    disp(['Exposure            : ',num2str(ref_cast_list(i).Exposure)]);
+    disp(['SMBase              : ',num2str(ref_cast_list(i).SMBase)]);
+    disp(['Image volume [L]    : ',num2str(ref_cast_list(i).img_vol_data)]);
+    disp(['Pixel        [µm]   : ',num2str(ref_cast_list(i).pix)]);
+    disp(['Pixel Area   [µm²]  : ',num2str(ref_cast_list(i).pix^2)]);
+    disp(['Aa                  : ',num2str(aa_data_ref)]);
+    disp(['Exp                 : ',num2str(ref_cast_list(i).expo_data)]);
+    disp('-------------------------------------------------------------------------');
 end
-disp(['Reference profile # : ',num2str(ref_cast.record)]);
-disp(['Observed volume [L] : ', num2str(sum(ref_cast.vol_ech(:,1), 'all'))]);
-disp(['Shutter             : ',num2str(ref_cast.ShutterSpeed)]);
-disp(['Gain                : ',num2str(ref_cast.gain)]);
-disp(['Threshold           : ',num2str(ref_cast.Thres)]);
-disp(['Exposure            : ',num2str(ref_cast.Exposure)]);
-disp(['SMBase              : ',num2str(ref_cast.SMBase)]);
-disp(['Image volume [L]    : ',num2str(ref_cast.img_vol_data)]);
-disp(['Pixel        [µm]   : ',num2str(ref_cast.pix)]);
-disp(['Pixel Area   [µm²]  : ',num2str(ref_cast.pix^2)]);
-disp(['Aa                  : ',num2str(aa_data_ref)]);
-disp(['Exp                 : ',num2str(ref_cast.expo_data)]);  
-disp('-------------------------------------------------------------------------');
+
+% adj uvp
 if contains(adj_cast.uvp,'uvp6'); aa_adj = round(aa_adj * 1000000);end    
 disp(['Adjusted UVP        : ',char(adj_cast.uvp)]);
 disp(['Light 1             : ',char(adj_cast.light1)]);
