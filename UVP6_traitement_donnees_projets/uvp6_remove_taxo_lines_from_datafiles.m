f3af5975e3eb87b2b8c9e84de8bf58d5b74b2e4e
picheral <marc.picheral@obs-vlfr.fr>
2021-11-16 17:28:58 +0100

    Corrections

--- UVP6_traitement_donnees_projets/uvp6_remove_taxo_lines_from_datafiles.m
+++ UVP6_traitement_donnees_projets/uvp6_remove_taxo_lines_from_datafiles.m
@@ -22,35 +22,33 @@ seq = dir([raw_folder ]);%'\20211101-045640']);
 
 %% Boucle sur les sequences
 for i=3:size(seq,1)
-%     data_folder = [raw_folder,'20211101-045640\'];
-%     data_folder = [raw_folder,'20211114-230530\'];
     data_folder = [raw_folder,seq(i).name,'\'];
     
     cd(data_folder)
-%     data_filename = ['20211101-045640_data.txt'];
-%     data_filename = ['20211114-230530_data.txt'];
+    %     data_filename = ['20211101-045640_data.txt'];
+    %     data_filename = ['20211114-230530_data.txt'];
     data_filename = [seq(i).name '_data.txt'];
     disp(data_filename)
     
+    % Copie de sauvegarde
+    backup_data_filename = ['backup_',data_filename];
+    eval(['copyfile ' data_filename ' ' backup_data_filename]);
+    
     %% read data lines from data file
-    [data, meta, taxo] = Uvp6DatafileToArray([data_folder, data_filename]);
+    [data, meta, taxo] = Uvp6DatafileToArray([data_folder, 'backup_', data_filename]);
     
     if ~isempty(meta)
         %% read HW and ACQ lines from data file
-        [HWline, Empty_line, ACQline, Taxoline] = Uvp6ReadMetalinesFromDatafile([data_folder, data_filename]);
-        
-        % Copie de sauvegarde
-        backup_data_filename = ['backup_',data_filename];
-        eval(['copyfile ' data_filename ' ' backup_data_filename]);
-        
+        [HWline, Empty_line, ACQline, Taxoline] = Uvp6ReadMetalinesFromDatafile([data_folder, 'backup_', data_filename]);
+
         % Ecriture dans fichier sans les lignes taxo
         corr_data_file = fopen([data_filename],'w');
         fprintf(corr_data_file,'%s\n',char(HWline));
         fprintf(corr_data_file,'%s\n',char(Empty_line));
         fprintf(corr_data_file,'%s\n',char(ACQline));
-        fprintf(corr_data_file,'%s\n',char(Empty_line));
         
-        for k = 1 : size(data,1)
+        for k = 1 : size(data,1) - 1
+            fprintf(corr_data_file,'%s\n',char(Empty_line));
             fprintf(corr_data_file,'%s\n',[char(meta(k)),':',char(data(k))]);
         end
         
