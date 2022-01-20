8c60f6e1dd55ad70a4b4d10ddbd0068f5ac6c80a
ccatalano <camille.catalano@obs-vlfr.fr>
2022-01-20 18:16:36 +0100

    compar uvp6 float : plot total nb of lpm ab

--- UVP6_traitement_donnees_projets/float_vs_uvp6/uvp6_compar_float_uvp6.m
+++ UVP6_traitement_donnees_projets/float_vs_uvp6/uvp6_compar_float_uvp6.m
@@ -22,16 +22,20 @@ raw_folder = fullfile(project_folder,'\raw\');
 
 disp("Selection of the LPM csv file from float")
 [float_filename, float_folder] = uigetfile('*.csv','Select the LPM csv file from float');
+% read CSV
 [park_lpm_table, ascent_lpm_table, surface_lpm_table] = Uvp6ReadLpmFromFloatLpmCSV(fullfile(float_folder, float_filename));
 float_lpm_table = ascent_lpm_table;
+% build num arrays
 [float_lpm_ab, float_lpm_grey] = Uvp6BuildLpmArrayFromFloatLpm(float_lpm_table);
 float_lpm_ab(:,6) = [];
 float_lpm_grey(:,6) = [];
 
 disp("Selection of the TAXO csv file from float")
 [float_filename, float_folder] = uigetfile('*.csv','Select the TAXO csv file from float');
+% read csv
 [park_taxo_table, ascent_taxo_table] = Uvp6ReadTaxoFromFloatTaxoCSV(fullfile(float_folder, float_filename));
 float_taxo_table = ascent_taxo_table;
+% build num array
 [float_taxo_ab, float_taxo_vol, float_taxo_grey] = Uvp6BuildTaxoArrayFromFloatTaxo(float_taxo_table);
 disp('---------------------------------------------------------------')
 
@@ -39,35 +43,19 @@ disp('---------------------------------------------------------------')
 %% selection of uvp6 data
 disp("Selection of the data file from uvp6")
 [uvp6_filename, uvp6_folder] = uigetfile('*.txt','Select the data file from uvp6');
-% read the lpm class limit
-[hw_line, ~, ~, ~] = Uvp6ReadMetalinesFromDatafile(fullfile(uvp6_folder, uvp6_filename));
-hw_array = strsplit(hw_line, ',');
-Aa = str2double(hw_array{19});
-Exp = str2double(hw_array{20});
 % read lpm data
 [data, meta, taxo] = Uvp6DatafileToArray(fullfile(uvp6_folder, uvp6_filename));
+% build taxo num array
 [uvp6_taxo_ab, uvp6_taxo_vol, uvp6_taxo_grey] = Uvp6ReadTaxoFromTaxotable(meta, data, taxo);
+% read data
 [uvp6_time_data, uvp6_depth_data, uvp6_raw_nb, uvp6_black_nb, uvp6_image_status] = Uvp6ReadDataFromDattable(meta, data); %%%%% remonter grey, attention dependency
-%%%% num arrays propres sans nan et par classe
-% uvp6_lpm_ab [pressure time image_nb class_nb]
+% build num arrays
 uvp6_lpm_ab = Uvp6BuildLpmArrayFromUvp6Lpm(uvp6_time_data, uvp6_depth_data, uvp6_raw_nb);
-% uvp6_lpm_grey [pressure time image_nb raw_grey]
 %uvp6_lpm_grey = Uvp6BuildLpmArrayFromUvp6Lpm(uvp6_time_data, uvp6_depth_data, uvp6_raw_grey);
 
-% read the lpm class limit
+% build the lpm class vector
 [hw_line, ~, ~, ~] = Uvp6ReadMetalinesFromDatafile(fullfile(uvp6_folder, uvp6_filename));
-hw_array = strsplit(hw_line, ',');
-hw_array(end) = {hw_array{end}(1:end-1)};
-esd_classes = str2double(hw_array(26:end));
-Aa = str2double(hw_array{19});
-Exp = str2double(hw_array{20});
-pixsize = [1:size(uvp6_lpm_ab(:,4:end),2)];
-esd_calib = 2*((Aa*(pixsize.^Exp)./pi).^0.5);
-uvp6_lpm_ab_cal = zeros(size(uvp6_lpm_ab(:,4:end),1), size(esd_classes,2)-1);
-for i=1:size(uvp6_lpm_ab(:,4:end),1)
-    uvp6_lpm_ab_cal(i,:) = sum_ab_classe(esd_calib, esd_classes, uvp6_lpm_ab(i,4:end));
-end
-uvp6_lpm_ab_cal = [uvp6_lpm_ab(:,1:3) uvp6_lpm_ab_cal];
+uvp6_lpm_ab_cal = Uvp6ClassDispatcher(hw_line, uvp6_lpm_ab);
 disp('------------------------------------------------------')
 
 
@@ -97,23 +85,43 @@ saveas(gcf, fullfile(project_folder, 'results', 'TAXO_object_nb_tot.png'))
 
 
 %% plots total nb of lpm
-%%%% plots total nb of lpm
+%concatenation nb of part
+uvp6_lpm_ab_cal_tot = sum(uvp6_lpm_ab_cal(:,4:end), 1);
+float_lpm_ab_tot = sum(float_lpm_ab(:,5:end), 1);
 
+%plots
+figure
+subplot(1,2,1)
+plot(uvp6_lpm_ab_cal_tot, 'r')
+hold on
+plot(float_lpm_ab_tot, 'g')
+xlabel('nb of the part class')
+ylabel('nb of particles of this class')
+legend('uvp6', 'float')
+title('total nb of particles in the profile')
+
+subplot(1,2,2)
+plot(uvp6_lpm_ab_cal_tot - float_lpm_ab_tot)
+xlabel('nb of the part class')
+ylabel('nb of particles of this class')
+title('difference between uvp6 and float')
+
+saveas(gcf, fullfile(project_folder, 'results', 'LPM_object_nb_tot.png'))
 
 %}
 %% concatenation of slices
-%%%% slicer
-% uvp6_taxo_ab_slices
-% uvp6_taxo_grey_slices
-% uvp6_taxo_vol_slices
-% uvp6_lpm_ab_slices
-% uvp6_lpm_grey_slices
-
+% ab
 uvp6_taxo_ab_slices = Uvp6FloatSlicer(uvp6_taxo_ab);
+
+% vol
 uvp6_taxo_vol_slices = Uvp6FloatSlicer(uvp6_taxo_vol);
+% average of volume
 uvp6_taxo_vol_slices(:,4:end) = uvp6_taxo_vol_slices(:,4:end) ./ uvp6_taxo_ab_slices(:,4:end);
 uvp6_taxo_vol_slices(isnan(uvp6_taxo_vol_slices)) = 0;
+
+% grey
 uvp6_taxo_grey_slices = Uvp6FloatSlicer(uvp6_taxo_grey);
+% average of grey
 uvp6_taxo_grey_slices(:,4:end) = uvp6_taxo_grey_slices(:,4:end) ./ uvp6_taxo_ab_slices(:,4:end);
 uvp6_taxo_grey_slices(isnan(uvp6_taxo_grey_slices)) = 0;
 
@@ -168,6 +176,7 @@ end
 
 
 %% plots profile per lpm class
+% concatenation of slices
 uvp6_lpm_ab_slices = Uvp6FloatSlicer(uvp6_lpm_ab);
 for j=1:12
     figure
