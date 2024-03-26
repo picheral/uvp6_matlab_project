629de7b42672f17e88fa46544b967ad5ec92ed25
ccatalano <camille.catalano@obs-vlfr.fr>
2024-03-26 16:40:24 +0100

    change data.txt format script and functions

--- UVP6_traitement_donnees_projets/uvp6_from2022formatTOold.m
+++ UVP6_traitement_donnees_projets/uvp6_from2022formatTOold.m
@@ -23,51 +23,17 @@ disp("Selection of the data file to downgrade")
 disp("Selected data file : " + data_folder + data_filename)
 disp('------------------------------------------------------')
 
+From2022formatToOldUVPData(data_folder, data_filename)
 
-%% read HW and ACQ lines from data file
-[HWline, line, ACQline, ~] = Uvp6ReadMetalinesFromDatafile([data_folder, data_filename]);
-
-%% read data lines from data file
-[data, meta] = Uvp6DatafileToArray([data_folder, data_filename]);
-
-%% Save old file and create new one
-disp('Save old file and create new one')
-rename = fullfile(data_folder, [data_filename(1:end-4) '_2022format.txt']);
-[~] = movefile(fullfile(data_folder, data_filename), rename, 'f');
-old_standard_file = fopen(fullfile(data_folder, data_filename), 'w');
-disp('------------------------------------------------------')
-
-%% Compute new hwline and new acqline
-disp('Computing new meta lines')
-HWline = split(HWline, ',');
-new_HWline = {HWline{1:7} '0' HWline{8:13} '193.49.112.100' HWline{14:end}}';
-new_HWline = join(new_HWline, ',');
-ACQline = split(ACQline, ',');
-new_ACQline = {ACQline{1:5} '1' ACQline{6:9} '10' ACQline{10:16} '0' ACQline{17:end}}';
-new_ACQline = join(new_ACQline, ',');
-disp('------------------------------------------------------')
-
-
-%% write HW and ACQ lines
-disp('writing new file')
-fprintf(old_standard_file,'%s\n',string(new_HWline));
-fprintf(old_standard_file,'%s\n',line);
-fprintf(old_standard_file,'%s\n',string(new_ACQline));
-
-    
-
-%% write data lines in file
-data_table = join([meta,data],':');
-for line_nb = 1:size(data_table,1)
-    fprintf(old_standard_file,'%s\n',line);
-    fprintf(old_standard_file,'%s\n',string(data_table(line_nb)));
-end
-fprintf(old_standard_file,'%s\n',line);
-disp("Modified file with old standard : " + fullfile(data_folder, data_filename))
-disp("Old file with 2022 standard : " + rename)
-fclose(old_standard_file);
 disp('------------------------------------------------------')
 
 
 disp('end of process')
 disp('------------------------------------------------------')
+
+
+
+
+
+
+
