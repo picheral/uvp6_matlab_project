889e1b7ec76f1886839457f795c1f48fbe850e21
marcp <marc.picheral@imev-mer.fr>
2025-04-14 17:20:29 +0200

    Ajout saisie de commentaire dans le MLX calibration report

--- UVP5_mise_base/UVP5_filtrage_data_images/UVP5_filtrage_artefacts/Filtrage_artefacts_main_search_zone.m
+++ UVP5_mise_base/UVP5_filtrage_data_images/UVP5_filtrage_artefacts/Filtrage_artefacts_main_search_zone.m
@@ -2,12 +2,14 @@
 % Leandro Ticlio et Marc Picheral
 % To REDO the full operation, copy the BRU files from the WORK folders into
 % the Results folder
+% the code uses the files from the results folder
 
 clc
 clear all
 close all
 
-project_folder_ref = uigetdir('', 'Select UVP5 project directory ');
+% project_folder_ref = uigetdir('', 'Select UVP5 project directory ');
+project_name = uigetdir('', 'Select UVP5 project directory ');
 
 fprintf('=========================================================== \n');
 fprintf('----------------------------------------------------------- \n');
@@ -32,7 +34,8 @@ fprintf('PLOTTING CHARTS TO DETECT impacted zone \n');
 s5_heatmap_rois_indat_size_nfilter_bounder(project_name,'initial')
 
 fprintf('=========================================================== \n');
-fprintf('Set the shape_xx and shape_yy vectors to adapt the zone to remove from the observations in the fig files (map_filter folder) \n');
+fprintf('Define the shape_xx and shape_yy vectors to adapt the zone to remove from the observations in the fig files (map_filter folder) \n');
+fprintf('Set ')
 fprintf('Modify the volume in the header file and the uvp5_configuration_data.txt file \n');
 fprintf('Create the header.xls file in the meta folder by adding a last column for the min object size \n');
 fprintf('The tool is paused \nPress Enter to continue when ready\n');
